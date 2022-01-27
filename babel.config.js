6c518c958553bf460177e33fbff69047cb6aa760
Marius Colacioiu <marius.colacioiu@gmail.com>
2022-01-28 00:11:52 +0100

    Add react-native-paper library

--- babel.config.js
+++ babel.config.js
@@ -8,7 +8,8 @@ module.exports = function (api) {
       ['@babel/plugin-proposal-class-properties', { loose: true }],
       ['@babel/plugin-proposal-private-methods', { loose: true }],
       ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-      'react-native-reanimated/plugin'
-    ]
-  }
-}
+      'react-native-reanimated/plugin',
+      'react-native-paper/babel',
+    ],
+  };
+};
