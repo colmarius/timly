e24fe127c234ab3ab706f5dddb3e623cc231a9e5
Marius Colacioiu <marius.colacioiu@gmail.com>
2022-01-28 00:52:15 +0100

    First test: some components for testing

--- packages/app/src/HelloWorld.tsx
+++ packages/app/src/HelloWorld.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Avatar, Button, Card, Paragraph, Title } from 'react-native-paper';
 
+const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
+
+const MyComponent = () => (
+  <Card>
+    <Card.Title title="My Title" subtitle="My Subtitle" left={LeftContent} />
+    <Card.Content>
+      <Title>Card title</Title>
+      <Paragraph>Card content</Paragraph>
+    </Card.Content>
+    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+    <Card.Actions>
+      <Button>Cancel</Button>
+      <Button>Ok</Button>
+    </Card.Actions>
+  </Card>
+);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -11,6 +28,10 @@ const styles = StyleSheet.create({
 
 export const HelloWorld = () => (
   <View style={styles.container}>
-    <Text>Hello, World!</Text>
+    <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+      Press me
+    </Button>
+    <Text>Ciao mondo</Text>
+    <MyComponent />
   </View>
 );
